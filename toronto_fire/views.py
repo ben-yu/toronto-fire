ddc439e8378e2093448b3ce1c234bbb218beb587
ben-yu <benjamin.sichit.yu@gmail.com>
2014-09-21 19:26:47 -0400

    Initial Scatterplot graph to compare # of trucks and avg duration

--- toronto_fire/views.py
+++ toronto_fire/views.py
@@ -10,5 +10,7 @@ class IncidentsView(View):
 
     def get(self, request, *args, **kwargs):
         avg_num_of_trucks = Incident.objects.values('incident_type').annotate(number_of_units = Avg('number_of_units')).values('incident_type','number_of_units')
-        context = {'avg_num_of_trucks': json.dumps(list(avg_num_of_trucks))}
+        avg_trucks_and_duration = Incident.objects.values('alarm_level').annotate(number_of_units = Avg('number_of_units'),\
+            avg_duration = Avg('duration_in_min')).values('alarm_level','number_of_units','avg_duration') 
+        context = {'avg_num_of_trucks': json.dumps(list(avg_num_of_trucks)),'avg_trucks_and_duration':json.dumps(list(avg_trucks_and_duration))}
         return render(request, 'incidents.html', context)
